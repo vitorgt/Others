0bf843d38e8c3958a7e1d2db49add0162a22c7f4
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2017-11-21 11:33:26 -0200

    Update code.js

--- SSC0118/FSMSim/code.js
+++ SSC0118/FSMSim/code.js
@@ -2,16 +2,46 @@
 
 	$( ".btn" ).click(function() {
 		
-		var tipoEstado = $("input[name='tipo']:checked").val();
+		var tipoEstado = parseInt($("input[name='tipo']:checked").val());
 		var qtdEstados = parseInt($("option:selected").val());
-		var qtdEntradas = $("input[name='qtdEntrada']").val();
-		var qtdSaidas = $("input[name='qtdSaida']").val();
+		var qtdEntradas = parseInt($("input[name='qtdEntrada']").val());
+		var qtdSaidas = parseInt($("input[name='qtdSaida']").val());
 		
 		var tp = "Mealy";
 		if(tipoEstado == 1) tp = "Moore";
 		qtdEstados++;
 		
-		alert("Tipo: " + tp + " com " + qtdEstados + " estados, " + qtdEntradas +" entradas e " + qtdSaidas + " saidas");
+		//alert("Tipo: " + tp + " com " + qtdEstados + " estados, " + qtdEntradas +" entradas e " + qtdSaidas + " saidas");
+		
+		var bitsEstados = 1;
+		
+		if(qtdEstados != 1) {
+			bitsEstados = parseInt(Math.ceil(Math.log2(qtdEstados)));
+		}
+		
+		//alert("Bits para a representação de estado " + bitsEstados);
+		
+		var tabelaVerd = $("textarea[name='tabelaVerdade']").val();
+		
+		var vetorTop = tabelaVerd.replace( /\n/g, " " ).split(" ");
+		
+		alert(tabelaVerd + " " + vetorTop.length);
+		
+		var i;
+		for(i=0;i<vetorTop.length;i++){
+			alert(vetorTop[i]);
+		}
+		
+		var numCol = bitsEstados*2 + qtdEntradas + qtdSaidas;
+		var numLin = bitsEstados * 2^qtdEntradas;
+		//var pos = j + i*numCol; //formula para achar a posicao da matriz para um vetor
+		
+
+		alert(numCol);
+
+
+		
 	});
 
 });
+
