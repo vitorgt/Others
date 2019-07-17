04b25413ffe511c35e4fac4dfc4bbd2e8faed224
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2019-07-16 23:18:01 -0300

    Sierpinski carpet (fractal) drawing

---
+++ frac/frac.js
@@ -0,0 +1,41 @@
+ite = 1
+size = 0
+
+function setup() {
+  size = Math.min(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
+  frac(width / 2 - size / 2, 0, size, size, 0, 0);
+}
+
+function frac(x, y, w, h, i, f) {
+  if (f == 255) {
+    fill(f);
+    rect(x, y, w, h);
+  }
+  for (var j = 0; f != 255 && j < 3; j++) {
+    for (var k = 0; k < 3; k++) {
+      if (j == 1 && k == 1) {
+        frac(x + (k * w) / 3, y + (j * h) / 3, w / 3, h / 3, i, 255);
+      } else if (i + 1 < ite) {
+        frac(x + (k * w) / 3, y + (j * h) / 3, w / 3, h / 3, i + 1, f);
+      }
+    }
+  }
+}
+
+function draw() {
+  text(ite, ite * 15, 10)
+}
+
+function mousePressed() {
+  ite++;
+  frac(width / 2 - size / 2, 0, size, size, 0, 0);
+}
+
+function keyTyped() {
+  if (key === 's') {
+    save('frac.png');
+  }
+}
